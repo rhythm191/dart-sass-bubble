aeeb3c31b3a124ad949e506db47c45c4fb0562e8
rhythm191 <sac2ndg+develop@gmail>
2020-01-17 16:35:21 +0900

    feat: add animation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(value, index) in Array(100)" :key="index" :class="`animation${index + 1}`"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style lang="scss">
+@use "sass:math";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +21,61 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+@for $i from 1 through 100 {
+  $size: math.ceil(math.random() * 300px + 30px);
+  $color: rgb(math.random() * 255, math.random() * 255, math.random() * 255);
+
+
+  .animation#{$i} {
+
+    box-sizing: border-box;
+    position: absolute;
+    left: math.random() * 900px;
+    top: math.random() * 600px;
+    border-radius: 50%;
+
+    border-width: 0;
+    border-style: solid;
+    border-color: $color;
+    background-color: $color;
+    width: $size;
+    height: $size;
+    opacity: 1;
+    z-index: math.ceil(math.random() * 100);
+    animation: scale-#{$i} math.random() * 2 + 3s math.random() * 30 + 1s infinite normal
+  }
+
+  @keyframes scale-#{$i} {
+    0% {
+      transform: scale(0);
+      opacity: 1;
+      background-color: $color;
+      border-width: 0;
+    }
+    20%{
+      opacity: 1;
+      background-color: $color;
+      border-width: $size / 2;
+    }
+
+    40% {
+      opacity: .8;
+      background-color: transparent;
+    }
+
+    70% {
+      transform: scale(1);
+
+    }
+    100% {
+      opacity: 0;
+      transform: scale(1);
+    }
+  }
 }
 </style>
