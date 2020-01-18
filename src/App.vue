cd0f636c14d73bec59b6864bfc7d1b6f87e39225
rhythm191 <sac2ndg+develop@gmail>
2020-01-18 14:24:57 +0900

    feat: add social buttons

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,111 @@
 <template>
   <div id="app">
-    <div v-for="(value, index) in Array(100)" :key="index" :class="`animation${index + 1}`"></div>
+    <header>
+      <ul>
+        <li>
+          <a href="https://github.com/rhythm191/dart-sass-bubble"
+            ><img src="/github.svg" class="sns-icon" /><span class="sr-only"
+              >Github</span
+            ></a
+          >
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/intent/tweet?text=dart-sass-bubble&url=https://dart-sass-bubble.rhyztech.net/"
+            ><img src="/twitter.svg" class="sns-icon" /><span class="sr-only"
+              >Twitter</span
+            ></a
+          >
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/sharer.php?u=https://dart-sass-bubble.rhyztech.net/&t=dart-sass-bubble"
+            ><img src="/facebook.svg" class="sns-icon" /><span class="sr-only"
+              >facebook</span
+            ></a
+          >
+        </li>
+      </ul>
+    </header>
+    <div class="canvas">
+      <div
+        v-for="(value, index) in Array(100)"
+        :key="index"
+        :class="`animation${index + 1}`"
+      ></div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app'
-}
+  name: "app"
+};
 </script>
 
 <style lang="scss">
 @use "sass:math";
 
 #app {
-  position: relative;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  flex: 0 1 auto;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   width: 100vw;
   max-width: 900px;
   height: 100vh;
   margin: 0 auto;
+  @media screen and (max-width: 899px) {
+    overflow: hidden;
+  }
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 40px;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+
+    li {
+      display: inline-block;
+      margin-right: 24px;
+    }
+  }
+}
+
+.sns-icon {
+  width: 32px;
+  height: 32px;
+}
+
+.sr-only {
+  position: absolute !important;
+  width: 1px !important;
+  height: 1px !important;
+  padding: 0 !important;
+  margin: -1px !important;
+  overflow: hidden !important;
+  clip: rect(0, 0, 0, 0) !important;
+  white-space: nowrap !important;
+  border: 0 !important;
+}
+
+.canvas {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 
 @for $i from 1 through 100 {
@@ -47,7 +127,15 @@ export default {
     height: $size;
     opacity: 0;
     z-index: math.ceil(math.random() * 100);
-    animation: scale-#{$i} math.random() * 2 + 3s math.random() * 30 + 1s infinite normal;
+    animation: scale-#{$i}
+      math.random() *
+      2 +
+      3s
+      math.random() *
+      30 +
+      1s
+      infinite
+      normal;
   }
 
   @keyframes scale-#{$i} {
@@ -57,20 +145,19 @@ export default {
       background-color: $color;
       border-width: 0;
     }
-    20%{
+    20% {
       opacity: 1;
       background-color: $color;
       border-width: $size / 2;
     }
 
     40% {
-      opacity: .8;
+      opacity: 0.8;
       background-color: transparent;
     }
 
     70% {
       transform: scale(1);
-
     }
     100% {
       opacity: 0;
